0e0ac3cc228621a9f9ceaba1b93beb3a9bfef011
Sathyamano30 <94217503+Sathyamano30@users.noreply.github.com>
2021-12-01 19:51:22 -0800

    Update Project_config.h

--- 3_Implementation/inc/Project_config.h
+++ 3_Implementation/inc/Project_config.h
@@ -1,1 +1,25 @@
+#ifndef __PROJECT_CONFIG_H__
+#define __PROJECT_CONFIG_H__
+/**
+ * @file project_config.h
+ * @author Bharath.G ()
+ * @brief Configuration file to define pins and ports for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+/**
+ * Macro Definitions
+ */
+
+#define trigger 10
+#define echo 11
+#define motor 8
+#define buzzer 12
+
+#include <avr/io.h>
+
+#endif /* __PROJECT_CONFIG_H__ */
