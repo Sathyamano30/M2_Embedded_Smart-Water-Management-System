63e4d7b40bc9ac0ebc15c4b7951acdb8a2e965f1
Sathyamano30 <94217503+Sathyamano30@users.noreply.github.com>
2021-12-01 19:48:44 -0800

    Update Smart water.h

--- 3_Implementation/inc/Smart water.h
+++ 3_Implementation/inc/Smart water.h
@@ -1,1 +1,21 @@
+/**
+ * @file Smart water.h
+ * @author Sathiyasri.M ()
+ * @brief Project to Smart water an LED connected to AVR328 MCU GPIO Pin
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+/**
+ * Macro Definitions
+ */
+
+#include <LiquidCrystal.h>
+ 
+#define trigger 10
+#define echo 11
+#define motor 8
+#define buzzer 12
