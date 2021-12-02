296b37b363c857e5d80b996607b8e2140d38225c
Sathyamano30 <94217503+Sathyamano30@users.noreply.github.com>
2021-12-01 19:54:50 -0800

    Update User_utils.h

--- 3_Implementation/inc/User_utils.h
+++ 3_Implementation/inc/User_utils.h
@@ -1,1 +1,28 @@
+#ifndef __USER_UTILS_H__
+#define __USER_UTILS_H__
+/**
+ * @file user_utils.h
+ * @author Sathiyasr.M ()
+ * @brief User defined utilities used across the project
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "project_config.h"
 
+void setup()
+{
+ lcd.begin(16,2);
+ pinMode(trigger,OUTPUT);
+ pinMode(echo,INPUT);
+ pinMode(motor, OUTPUT);
+ pinMode(buzzer, OUTPUT);
+ lcd.print("  Water Level ");
+ lcd.setCursor(0,1);
+ lcd.print("   Indicator  ");
+ delay(2000);
+}
+
+#endif /* __USER_UTILS_H__ */
